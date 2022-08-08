04443e96ce65569bdfff88d009bef63da38ccfbf
Ifeanyi Nwachukwu <72893196+IfeanyiNwachukwu@users.noreply.github.com>
2022-08-08 18:19:40 +0100

    Completed Google API middleware

---
+++ MappingSOlution/MapSolution/GoogleMaps.Library/GoogleMap.Library/GoogleDistanceApi.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Text.Json;
+
+namespace GoogleMap.Library
+{
+    public class GoogleDistanceApi
+    {
+        private readonly IConfiguration _configuration;
+
+        public GoogleDistanceApi(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public async Task<GoogleDistanceData> GetMapDistance(string originCity, string destinationCity)
+        {
+            var apiKey = _configuration["googleDistanceApi:apiKey"];
+            var googleDistanceApiUrl = _configuration["googleDistanceApi:apiUrl"];
+            googleDistanceApiUrl += $"units=imperial&sensor=false&origins={originCity}&destinations={destinationCity}&key={apiKey}";
+
+            using var client = new HttpClient();
+
+            var request = new HttpRequestMessage(HttpMethod.Get, googleDistanceApiUrl);
+            var response = await client.SendAsync(request);
+            response.EnsureSuccessStatusCode();
+
+            await using var data = await response.Content.ReadAsStreamAsync();
+
+            var distanceInfo = await JsonSerializer.DeserializeAsync<GoogleDistanceData>(data);
+
+            return distanceInfo;
+
+
+        }
+    }
+}
---
+++ MappingSOlution/MapSolution/Mapped.API/Mapped.Api/Extensions/ServicesExtension.cs
@@ -0,0 +1,12 @@
+﻿using GoogleMap.Library;
+
+namespace Mapped.Api.Extensions
+{
+    public static class ServicesExtension
+    {
+        public static void RegisterGoogleDistanceApi(this IServiceCollection services)
+        {
+            services.AddTransient<GoogleDistanceApi>();
+        }
+    }
+}
