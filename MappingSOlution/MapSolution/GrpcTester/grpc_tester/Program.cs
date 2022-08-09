490635d0af568dc607b9e31faf0aa94da4445f48
Ifeanyi Nwachukwu <72893196+IfeanyiNwachukwu@users.noreply.github.com>
2022-08-09 04:27:38 +0100

    Added a console client to consume Grpc Service

---
+++ MappingSOlution/MapSolution/GrpcTester/grpc_tester/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Grpc.Net.Client;
+using Mapped.Api.Protos;
+
+
+    var channel = GrpcChannel.ForAddress(new Uri("https://localhost:7024"));
+    var client = new DistanceInfo.DistanceInfoClient(channel);
+
+    var response = await client.GetDistanceAsync(new Cities
+    {
+        OriginCity = "Topeka,KS",
+        DestinationCity = "Los Angeles,CA"
+    });
+
+    Console.WriteLine(response.Miles);
+
+    Console.ReadKey();
--- MappingSOlution/MapSolution/Mapped.API/Mapped.Api/Program.cs
+++ MappingSOlution/MapSolution/Mapped.API/Mapped.Api/Program.cs
@@ -1,10 +1,12 @@
 using Mapped.Api.Extensions;
+using Mapped.Api.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.RegisterGoogleDistanceApi();
 builder.Services.AddControllers();
+builder.Services.AddGrpc();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -21,7 +23,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
-
+app.MapGrpcService<DistanceInfoService>();
 app.MapControllers();
+
+
 
 app.Run();
